9b1a289187b138316f4b57f9b4a1e6abf26788f1
ALLaTk <regicter1810@mail.ru>
2022-05-16 00:27:45 +0300

    complete task

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+const pathFile = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(pathFile);
+
+stdout.write('Enter your text:\n')
+stdin.on('data', data => {
+  const text = data.toString().trim();
+  if (text === 'exit'){
+    stdout.write('Good bay!'); 
+    process.exit();
+  } else {
+    output.write(`${text} `)
+  } 
+});
+
+process.on('SIGINT', () => {
+   stdout.write('Good bay!') 
+   process.exit();
+});
+
+
+
+
