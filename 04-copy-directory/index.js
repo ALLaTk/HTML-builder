0ab7063ce8c6a3d7a793de5e4cbb079f4f741fa9
ALLaTk <regicter1810@mail.ru>
2022-05-16 22:56:51 +0300

    comlete task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const pathFile = path.join(__dirname, 'files');
+const pathFileCopy = path.join(__dirname, 'files-copy');
+const { stdout } = process;
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.readdir(pathFileCopy, {withFileTypes: true}, (err, data,) => {
+  if (err) throw err;
+
+  data.forEach(el => {
+    fs.unlink(path.join(pathFileCopy, el.name), (err) => {
+      if (err) throw err;
+    });
+  });
+});
+
+fs.readdir(pathFile, {withFileTypes: true}, (err, data,) => {
+  if (err) throw err;
+  data.forEach(el => {
+    if(el.isFile()) {
+      fs.copyFile(path.join(pathFile, el.name), path.join(pathFileCopy, el.name), (err) =>{
+        if (err) throw err;
+      });
+    } 
+    if(el.isDirectory()) {
+      stdout.write(`You created a folder '${el.name}'. You need to create a file!`);
+    }
+  });
+});
