09eebc19f507c1e405f92e6d4e460ddd67a9c514
ALLaTk <regicter1810@mail.ru>
2022-05-18 13:52:34 +0300

    refactor task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,37 +2,40 @@ const fs = require('fs');
 const path = require('path');
 const pathFile = path.join(__dirname, 'files');
 const pathFileCopy = path.join(__dirname, 'files-copy');
-const { stdout } = process;
 
-
-fs.mkdir(pathFileCopy, { recursive: true }, (err) => {
-  if (err) throw err;
-});
-
-fs.readdir(pathFileCopy, (err, data) => {
-  if (err) throw err;
-  data.forEach((elem) => {
-    fs.access(path.join(pathFile, elem), (err) => {
-      if (err) {
-        fs.rm(path.join(pathFileCopy, elem), { recursive: true, force: true}, (err) => {
+const copyDir = (pathFile, pathFileCopy) => {
+  
+  fs.mkdir(pathFileCopy, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  
+  fs.readdir(pathFileCopy, (err, data) => {
+    if (err) throw err;
+    data.forEach((elem) => {
+      fs.access(path.join(pathFile, elem), (err) => {
+        if (err) {
+          fs.rm(path.join(pathFileCopy, elem), { recursive: true, force: true}, (err)   => {
+            if (err) throw err;
+          });
+        }
+      });
+    });
+  });
+  
+  fs.readdir(pathFile, {withFileTypes: true}, (err, data,) => {
+    if (err) throw err;
+    data.forEach(el => {
+      if(el.isFile()) {
+        fs.copyFile(path.join(pathFile, el.name), path.join(pathFileCopy, el.name),   (err) =>{
           if (err) throw err;
         });
+      } 
+      if(el.isDirectory()) {
+        copyDir(path.join(pathFile, el.name), path.join(pathFileCopy, el.name));
       }
     });
   });
-});
+  
+};
 
-fs.readdir(pathFile, {withFileTypes: true}, (err, data,) => {
-  if (err) throw err;
-  data.forEach(el => {
-    if(el.isFile()) {
-      fs.copyFile(path.join(pathFile, el.name), path.join(pathFileCopy, el.name), (err) =>{
-        if (err) throw err;
-      });
-    } 
-    if(el.isDirectory()) {
-      stdout.write(`You created a folder '${el.name}'. You need to create a file!`);
-    }
-  });
-});
-
+copyDir(pathFile, pathFileCopy);
