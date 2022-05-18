dd1891975ca16beba034cc1d52dabb9b48f47410
ALLaTk <regicter1810@mail.ru>
2022-05-18 21:51:57 +0300

    complete task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const pathDir = path.join(__dirname, 'styles');
+const pathNewFile = path.join(__dirname, 'project-dist', 'bundle.css');
+const output = fs.createWriteStream(pathNewFile, 'utf-8');
+
+fs.readdir(pathDir, {withFileTypes: true}, (err, data) => {
+  
+  if (err) throw err;
+  data.forEach(elem => {
+    if(elem.isFile()) {
+      if(path.extname(elem.name) === '.css') {
+        const pathElem = path.join(pathDir, elem.name);
+        const readFile = fs.createReadStream(pathElem, 'utf-8');
+        readFile.pipe(output);
+      }
+    } 
+  });
+});    
+
