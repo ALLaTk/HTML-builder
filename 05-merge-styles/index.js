974ce63f90cca67967cb0ea4b841010df6f2fd3a
ALLaTk <regicter1810@mail.ru>
2022-05-21 19:16:45 +0300

    refactor task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,20 +1,24 @@
 const fs = require('fs');
+const promise = require('fs/promises');
 const path = require('path');
 const pathDir = path.join(__dirname, 'styles');
 const pathNewFile = path.join(__dirname, 'project-dist', 'bundle.css');
-const output = fs.createWriteStream(pathNewFile, 'utf-8');
+const style = fs.createWriteStream(pathNewFile, 'utf-8');
 
-fs.readdir(pathDir, {withFileTypes: true, recursive: true, force: true}, (err, data) => {
-  
-  if (err) throw err;
-  for(let elem of data) {
-    if(elem.isFile()) {
-      if(path.extname(elem.name) === '.css') {
+const buildFiles = async () => {
+
+  try {
+    const stylesComponents = await promise.readdir(pathDir, {recursive: true,   force:   true, withFileTypes: true});
+    for (let elem of stylesComponents) {
+      if(elem.isFile() && path.extname(elem.name) === '.css') {
         const pathElem = path.join(pathDir, elem.name);
-        const readFile = fs.createReadStream(pathElem, 'utf-8');
-        readFile.pipe(output);
-      }
-    } 
-  };
-});    
+        const readElem = await promise.readFile(pathElem, {recursive: true, force:   true});
+        style.write(`${readElem}\n\n`);
+      } 
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+buildFiles();
